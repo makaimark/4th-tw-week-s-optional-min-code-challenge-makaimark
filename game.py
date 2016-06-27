ae59739b848282b8c250a7682259b3889a4fc4bb
makaimark <makaimark@gmail.com>
2016-06-27 09:56:39 +0200

    Update game.py

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ def g(b):
     for i in range(10):
         a, c = 0, random.randint(1, b)
         while c != a:
-            a = int(input("1 to " + str(b) + ":"))
-            print("low" if a < c else "high" if a > c else "ok")
+            a = int(input("Enter an integer from 1 to " + str(b) + ":"))
+            print("guess is low" if a < c else "guess is high" if a > c else "you got it!")
 g(99)
 g(49)
