dd69e4b7d48d1347c029df2ade22d017e871109d
makaimark <makaimark@gmail.com>
2016-06-27 08:44:31 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,11 @@
 import random
 
 
-def game_logic(b):
+def g(b):
     for i in range(10):
         a, c = 0, random.randint(1, b)
         while c != a:
-            a = int(input("Enter an int from 1 to " + str(b) + ":"))
-            print("guess is low" if a < c else "guess is high" if a > c else "you guessed it!")
-game_logic(99)
-game_logic(49)
+            a = int(input("1 to " + str(b) + ":"))
+            print("low" if a < c else "high" if a > c else "ok")
+g(99)
+g(49)
